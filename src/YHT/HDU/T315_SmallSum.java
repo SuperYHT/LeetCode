fab37c9101ac38c7112a0fdc05a976bc86f40ed7
yht1997 <920891599@qq.com>
2022-09-22 23:43:01 +0800

    小和数 debug完成

--- src/YHT/HDU/T315_SmallSum.java
+++ src/YHT/HDU/T315_SmallSum.java
@@ -6,18 +6,19 @@ import java.util.List;
 import static jdk.nashorn.internal.objects.Global.print;
 
 public class T315_SmallSum {
-    private static int[] ans;
-    private static int[] index;
-    private static int[] tempindex;
+    public static int[] ans;
+    public static int[] index;
+    public static int[] tempindex;
     public  static List<Integer> countSmaller(int[] nums) {
         List<Integer> result = new ArrayList<Integer>();
         ans = new int[nums.length];
         index = new int[nums.length];
         tempindex = new int[nums.length];
-        process(nums,0,nums.length-1);
         for(int i=0;i<nums.length;i++){
             index[i] = i;
         }
+        process(nums,0,nums.length-1);
+
         for (int j:ans){
             result.add(j);
         }
@@ -43,25 +44,25 @@ public class T315_SmallSum {
         while (p <= mid && q<=r){
             if(nums[p] <= nums[q]){
                 tempindex[x] = index[p];
+                count = q -mid -1 ;
+                ans[index[p]] += count;
                 temp[i++] = nums[p++];
                 x++;
             }else {
                 tempindex[x] = index[q];
-                count = r - q +1;
-                ans[index[q]] += count;
                 temp[i++] = nums[q++];
                 x++;
             }
         }
         while (p <= mid){
+            count = q -mid -1 ;
             tempindex[x] = index[p];
+            ans[index[p]] += count;
             temp[i++] = nums[p++];
             x++;
         }
         while (q <= r){
-            count = r - q + 1;
             tempindex[x] = index[q];
-            ans[index[q]] += count;
             temp[i++] = nums[q++];
             x++;
         }
